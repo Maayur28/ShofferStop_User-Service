990965ce2caf6ba973959335b4e1cb7a6ee75fb4
Mayur Agarwal <mayuragarwal2812@gmail.com>
2023-12-20 11:08:57 +0530

    added fix

--- src/main/java/com/userservice/service/impl/WishlistServiceImpl.java
+++ src/main/java/com/userservice/service/impl/WishlistServiceImpl.java
@@ -11,8 +11,8 @@ import com.userservice.utils.JwtTokenUtil;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
